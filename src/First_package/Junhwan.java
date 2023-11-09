f8224ac870d05efce1e16f93d8f4c6af6cf67140
JunHwan <devjunsday@gmail.com>
2023-11-09 21:43:38 +0900

    1빠

--- src/First_package/Junhwan.java
+++ src/First_package/Junhwan.java
@@ -3,8 +3,23 @@ package First_package;
 public class Junhwan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	User me = new User("최준환","언어학과",201900607);
+	me.showinfo();
+	System.out.println("\n인삿말 : 내가 대장이다 크하하하하하하 부럽지");
+}
+	
+}
+class User{
+	private String name;
+	private String studies;
+	private int StudentId;
+	User(String name_,String studies_,int StudentId_){
+		this.name = name_;
+		this.studies = studies_;
+		this.StudentId = StudentId_;
 	}
-
+	public void showinfo() {
+		System.out.printf("이름 : %s \n학번 : %d\n학과 : %s",name,StudentId,studies);
+	}
+	
 }
